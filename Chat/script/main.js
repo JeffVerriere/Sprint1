24c71a292e0fc447c534063861f06ac7b09644f0
JeffVerriere <55962309+JeffVerriere@users.noreply.github.com>
2020-12-10 10:48:41 +0100

    Add files via upload

---
+++ Chat/script/main.js
@@ -0,0 +1,11 @@
+function check(age){
+	
+             if(age< 14)
+             {
+                alert("Vous êtes un méchant chien!");
+             }
+             else
+             {
+                alert("Vous êtes un gentil chaton!");
+             }
+         }
